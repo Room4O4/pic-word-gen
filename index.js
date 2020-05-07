eff700820254e2006578bda9adf8d745b4147ef0
Sampath Kumar Krishnan <sampath3493@gmail.com>
2020-05-07 22:36:44 +0530

    feat: Add default level to the generateWord function - default level is set as easy

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const words = require('./words.json');
 
 const MAX_WORD_INDEX = 50;
 const wordlevels = ['easy', 'medium', 'hard']
+const defaultLevel = 'easy';
 
 const generateWord = (level) => {
-    if(!wordlevels.includes(level)){
+    if(level && !wordlevels.includes(level)){
         return null;
     }
     const randomNumber = utils.generateRandomInt(MAX_WORD_INDEX);
-    return words[level][randomNumber];
+    return words[level || defaultLevel][randomNumber];
 }
 
 module.exports = {
